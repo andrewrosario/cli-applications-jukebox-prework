7cdbe7e0fb9aa678a0e11215c08d9f5063b4e8be
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-17 17:30:39 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -7,17 +7,6 @@ def help
 end
 
 def list(songs)
-  songs = [
-  "Phoenix - 1901",
-  "Tokyo Police Club - Wait Up",
-  "Sufjan Stevens - Too Much",
-  "The Naked and the Famous - Young Blood",
-  "(Far From) Home - Tiga",
-  "The Cults - Abducted",
-  "Phoenix - Consolation Prizes",
-  "Harry Chapin - Cats in the Cradle",
-  "Amos Lee - Keep It Loose, Keep It Tight"
-]
   songs.map.with_index { |item, index|
     puts "#{index + 1}. #{item}"
   }
