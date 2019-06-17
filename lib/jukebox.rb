994bcb68507d4dd97a7ad16ebb88bc858fca4413
Andrew Rosario <rosario.andrew@gmail.com>
2019-06-17 17:19:11 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -44,8 +44,8 @@ def play(songs)
   puts input.class
   
   songs.each_with_index { |song, index|
-    
-    if input == (((index + 1).to_s) || song.include? "#{input}")
+    puts song.class
+    if input == (index + 1).to_s # || song.include? "#{input}")
       puts "#{song} is now playing"
     end
   }
